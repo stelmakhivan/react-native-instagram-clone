a19ee64ab91012fe082c1678d27e8041a38c8b98
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-05 08:12:21 +0200

    GraphQL schema initial setup

--- src/server.js
+++ src/server.js
@@ -2,21 +2,11 @@ require('dotenv').config()
 import { GraphQLServer } from 'graphql-yoga'
 import logger from 'morgan'
 
+import schema from './schema'
+
 const PORT = process.env.PORT || 4000
 
-const typeDefs = `
-  type Query{
-    hello: String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hi'
-  }
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
+const server = new GraphQLServer({ schema })
 
 server.express.use(logger('dev'))
 
