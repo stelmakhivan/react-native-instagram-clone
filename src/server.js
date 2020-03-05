6a9cc155427d9cf2c926cbc741a918e34fdfeb89
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-05 07:41:21 +0200

    Initial setup nodemon + server

---
+++ src/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+import { GraphQLServer } from 'graphql-yoga'
+import logger from 'morgan'
+
+const PORT = process.env.PORT || 4000
+
+const typeDefs = `
+  type Query{
+    hello: String!
+  }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hi'
+  }
+}
+
+const server = new GraphQLServer({ typeDefs, resolvers })
+
+server.express.use(logger('dev'))
+
+server.start({ port: PORT }, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+)
