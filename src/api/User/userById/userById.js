d1afbe527c0ff76bebd517db1836d613b1e9b8ed
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-07 19:52:59 +0200

    Integrate Prisma with api graphql

---
+++ src/api/User/userById/userById.js
@@ -0,0 +1,9 @@
+import { prisma } from '../../../../generated/prisma-client'
+export default {
+  Query: {
+    userById: async (_, args) => {
+      const { id } = args
+      return await prisma.user({ id }).$fragment()
+    }
+  }
+}
