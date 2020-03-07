82b0230646dee22f11f5ccca3c42f80c0edcbebe
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-07 19:40:00 +0200

    Removed Greetings

--- src/api/Greetings/sayHello/sayHello.js
+++
@@ -1,9 +0,0 @@
-import { prisma } from '../../../../generated/prisma-client/index'
-export default {
-  Query: {
-    sayHello: async () => {
-      console.log(await prisma.users())
-      return 'Hello'
-    }
-  }
-}
