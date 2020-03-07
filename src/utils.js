e0d4f420f2b28e530baf46851725918ebf6f5d12
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-07 23:38:54 +0200

    SendGrid Api to send emails

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,24 @@
+import dotenv from 'dotenv'
+import path from 'path'
+dotenv.config({ path: path.resolve(__dirname, '.env') })
+
+import sgMail from '@sendgrid/mail'
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
 import { adjectives, nouns } from './words'
 
 export const generateSecret = () => {
   const randomNumber = Math.floor(Math.random() * adjectives.length)
   return `${adjectives[randomNumber]} ${nouns[randomNumber]}`
+}
+
+export const sendSecretMail = (address, secret) => {
+  const email = {
+    from: `React Native Instagram Clone <${process.env.GOOGLE_ACCOUNT}>`,
+    to: address,
+    subject: 'Login Secret for React Native Instagram Clone',
+    html: `Hello! Your login secret is ${secret}<br/>Copy paste on the app/website to log in`
+  }
+
+  return sgMail.send(email)
 }
