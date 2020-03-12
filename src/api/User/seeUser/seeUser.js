a13721db423f21a09872ca656abb4e4b546d8f3b
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-12 07:45:43 +0200

    Refactored see user/my profile resolvers

--- src/api/User/seeUser/seeUser.js
+++ src/api/User/seeUser/seeUser.js
@@ -1,9 +1,11 @@
 import { prisma } from '../../../../generated/prisma-client'
 export default {
   Query: {
-    seeUser: (_, args, { request }) => {
+    seeUser: async (_, args) => {
       const { id } = args
-      return prisma.user({ id })
+      const user = await prisma.user({ id })
+      const posts = await prisma.user({ id }).posts()
+      return { user, posts }
     }
   }
 }
