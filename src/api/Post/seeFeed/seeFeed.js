7947f3b1f612b86f1099f038a949b813a7ec816a
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-17 19:20:30 +0200

    seeFeed resolver

---
+++ src/api/Post/seeFeed/seeFeed.js
@@ -0,0 +1,17 @@
+import { isAuthenticated } from '../../../middlewares'
+import { prisma } from '../../../../generated/prisma-client'
+export default {
+  Query: {
+    seeFeed: async (_, __, { request }) => {
+      isAuthenticated(request)
+      const { user } = request
+      const following = await prisma.user({ id: user.id }).following()
+      return prisma.posts({
+        where: {
+          user: { id_in: [...following.map(user => user.id), user.id] },
+        },
+        orderBy: 'createdAt_DESC',
+      })
+    },
+  },
+}
