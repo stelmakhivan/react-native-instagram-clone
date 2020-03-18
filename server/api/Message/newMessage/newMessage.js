f0970fd7db0e804b55f840051cc445ac58d9d45a
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-18 08:56:17 +0200

    newMessage resolver

---
+++ server/api/Message/newMessage/newMessage.js
@@ -0,0 +1,27 @@
+import { prisma } from '../../../../generated/prisma-client'
+
+export default {
+  Subscription: {
+    newMessage: {
+      subscribe: (_, args) => {
+        const { roomId } = args
+        return prisma.$subscribe
+          .message({
+            AND: [
+              { mutation_in: 'CREATED' },
+              {
+                node: {
+                  room: { id: roomId },
+                },
+              },
+            ],
+          })
+          .node()
+      },
+      resolve: (payload, args, context) => {
+        console.log(args, context)
+        return payload
+      },
+    },
+  },
+}
