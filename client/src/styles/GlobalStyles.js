ecb2390f3194a75ebd3dd4005674c1aee0ee1de9
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-03-21 15:52:20 +0200

    Fixed global styles warning

--- client/src/styles/GlobalStyles.js
+++ client/src/styles/GlobalStyles.js
@@ -3,7 +3,6 @@ import reset from 'styled-reset'
 
 export default createGlobalStyle`
   ${reset};
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600,700');
   * {
     box-sizing: border-box
   }
