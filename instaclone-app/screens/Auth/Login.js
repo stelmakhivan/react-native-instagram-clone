ea2d7877b7072f2923ac7f0e7d898f90a7a5e7e3
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-04-25 18:32:59 +0300

    basic email validation; keyboard.dismiss in login screen

--- instaclone-app/screens/Auth/Login.js
+++ instaclone-app/screens/Auth/Login.js
@@ -1,5 +1,8 @@
 import React, { useCallback } from 'react'
+import { Alert, TouchableWithoutFeedback, Keyboard } from 'react-native'
+
 import styled from 'styled-components/native'
+
 import AuthButton from '../../components/AuthButton'
 import AuthInput from '../../components/AuthInput'
 import useInput from '../../hooks/useInput'
@@ -10,18 +13,36 @@ const View = styled.View`
   flex: 1;
 `
 
+const EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
 const Login = () => {
   const emailInput = useInput('')
-  const handleLogin = useCallback(() => {}, [])
+
+  const handleLogin = useCallback(async () => {
+    const { value } = emailInput
+    if (value === '') {
+      return Alert.alert("Email can't be empty")
+    } else if (!value.includes('@') || !value.includes('.')) {
+      return Alert.alert('Please write an email')
+    } else if (!EMAIL_REGEX.test(value)) {
+      return Alert.alert('That email is invalid')
+    }
+  }, [emailInput])
+
   return (
-    <View>
-      <AuthInput
-        {...emailInput}
-        placeholder="Email"
-        keyboardType="email-address"
-      />
-      <AuthButton onPress={handleLogin} text="Log In" />
-    </View>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <View>
+        <AuthInput
+          {...emailInput}
+          placeholder="Email"
+          keyboardType="email-address"
+          returnKeyType="send"
+          onSubmitEditing={handleLogin}
+          autoCorrect={false}
+        />
+        <AuthButton onPress={handleLogin} text="Log In" />
+      </View>
+    </TouchableWithoutFeedback>
   )
 }
 
