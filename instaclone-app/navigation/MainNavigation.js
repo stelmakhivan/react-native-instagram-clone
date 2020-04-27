89f35df1a6d320db21bde2b5fbc77342e137577e
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-04-27 22:50:16 +0300

    Configure other stack navigators

--- instaclone-app/navigation/MainNavigation.js
+++ instaclone-app/navigation/MainNavigation.js
@@ -6,10 +6,14 @@ const Stack = createStackNavigator()
 import PhotoNavigation from './PhotoNavigation'
 import TabNavigation from './TabNavigation'
 import MessageNavigation from './MessageNavigation'
+import { stackStyles } from './config'
 
 const MainNavigation = () => {
   return (
-    <Stack.Navigator headerMode="none" mode="modal">
+    <Stack.Navigator
+      headerMode="none"
+      mode="modal"
+      screenOptions={{ headerStyle: stackStyles }}>
       <Stack.Screen name="TabNavigation" component={TabNavigation} />
       <Stack.Screen name="PhotoNavigation" component={PhotoNavigation} />
       <Stack.Screen name="MessageNavigation" component={MessageNavigation} />
--- instaclone-app/navigation/MessageNavigation.js
+++ instaclone-app/navigation/MessageNavigation.js
@@ -6,10 +6,11 @@ const Stack = createStackNavigator()
 
 import Messages from '../screens/Messages/Messages'
 import Message from '../screens/Messages/Message'
+import { stackStyles } from './config'
 
 const MessageNavigation = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{ headerStyle: stackStyles }}>
       <Stack.Screen name="Messages" component={Messages} />
       <Stack.Screen name="Message" component={Message} />
     </Stack.Navigator>
