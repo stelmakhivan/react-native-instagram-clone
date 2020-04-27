ed83fd989af413ae49fc7dc2e98034d2659d9773
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-04-27 21:55:33 +0300

    removed asyncstorage clear()

--- instaclone-app/App.js
+++ instaclone-app/App.js
@@ -32,7 +32,6 @@ export default function App() {
       })
       await Asset.loadAsync([require('./assets/logo.png')])
 
-      await AsyncStorage.clear()
       const cache = new InMemoryCache()
       await persistCache({
         cache,
