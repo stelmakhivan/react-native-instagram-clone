d2a6850adac06993e4fa3760a81b04734bc8429b
Ivan Stelmakh <stelmakh.developer@gmail.com>
2020-05-01 15:57:02 +0300

    SquarePhoto refactoring

--- instaclone-app/components/SquarePhoto.js
+++ instaclone-app/components/SquarePhoto.js
@@ -1,20 +1,28 @@
-import React from 'react'
-import { TouchableOpacity, Image } from 'react-native'
+import React, { useCallback } from 'react'
+import { TouchableOpacity, Image, StyleSheet } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 import PropTypes from 'prop-types'
 import constants from '../constants'
 
 const SquarePhoto = ({ files = [], id }) => {
   const navigation = useNavigation()
+  const navigateTo = useCallback(() => navigation.navigate('Detail', { id }), [
+    id,
+    navigation,
+  ])
   return (
-    <TouchableOpacity onPress={() => navigation.navigate('Detail', { id })}>
-      <Image
-        source={{ uri: files[0].url }}
-        style={{ width: constants.width / 3, height: constants.height / 6 }}
-      />
+    <TouchableOpacity onPress={navigateTo}>
+      <Image source={{ uri: files[0].url }} style={styles.image} />
     </TouchableOpacity>
   )
 }
+
+const styles = StyleSheet.create({
+  image: {
+    width: constants.width / 3,
+    height: constants.height / 6,
+  },
+})
 
 SquarePhoto.propTypes = {
   files: PropTypes.arrayOf(
